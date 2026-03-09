4b4b4330a9170de8d4884fef0d7b9c4127e8ea3b
Diego Navas <diego.navas.martinez@alumnos.upm.es>
2026-03-09 13:19:46 +0100

    Modificacion de archivos de la practica 2

--- Practica2/Web/Web.c
+++ Practica2/Web/Web.c
@@ -21,8 +21,8 @@ typedef struct _imageFileItem {
 
 #define IMAGE_FILE_COUNT 21U
 
-/* Last-Modified: Mon,    Mar 2026 12:10:28 GMT */
-const uint32_t imageLastModified = 1773058228U;
+/* Last-Modified: Mon,    Mar 2026 12:17:22 GMT */
+const uint32_t imageLastModified = 1773058642U;
 
 static const uint8_t imageFileData[29282U] = {
 
